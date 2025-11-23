c678a9cdbdca60245d337c858f0e961d4c8e77da
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2025-11-23 15:09:40 +0500

    'added_veriable'

--- main.py
+++ main.py
@@ -6,17 +6,61 @@ Created on Sat Nov 22 11:14:53 2025
 """
 
 # sana 23.11.2025
+
+# print arifmetik amallar
 import math
 
-print ('"Nexia", "Tiko", "Damas" ko\'rganlar havas qilar')
+#print ('"Nexia", "Tiko", "Damas" ko\'rganlar havas qilar')
+
 # 5 ning 4-darajasini toping
-print('5 ning 4-darajasi', 5**4)
+#print('5 ning 4-darajasi', 5**4)
+
 # 22 ni 4 ga bolganda qancha qoldiq qoladi
-print('22 ni 4 ga bolsa', 22/4 ,'qoldiq chiqadi')
+#print('22 ni 4 ga bolsa', 22/4 ,'qoldiq chiqadi')
+
 # tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
-print("tomonlari 125 ga teng kvadratning yuzi", 125*125, "perimetri", 125*4)
+#print("tomonlari 125 ga teng kvadratning yuzi", 125*125, "perimetri", 125*4)
+
 # diametri 12 teng bolgan doiraning yuzini toping
-print('diametri 12 teng bolgan doiraning yuzi', 3.14*(12/2)**2)
+#print('diametri 12 teng bolgan doiraning yuzi', 3.14*(12/2)**2)
+
 # Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping
-print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", math.sqrt((6*6) + (7*7)))
-print("hello Ayubxon")
+#print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", math.sqrt((6*6) + (7*7)))
+
+
+# /////////////////
+
+# ism = "Abdulloh"
+# yosh = 25
+# print(ism)
+# ism = "Muhammad"
+# print(ism)
+# print(yosh)
+
+# a = 6
+# b = 7
+# c = (a+b)**2
+# print(c)
+
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+text = "hello world!"
+print(text)
+
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar = "bugun havo ochiq boldi"
+print(xabar)
+xabar = 'kechga borib yomg\'ir yog\'di'
+print(xabar)
+# class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+# class = "11-a"
+# print(class)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi", radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+
+
+
+
