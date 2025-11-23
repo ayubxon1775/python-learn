f80c6ac9ac3a57f7e2dbbc56b6191aa7c623fb75
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2025-11-23 19:24:25 +0500

    'string_and_string_method'

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ import math
 
 # /////////////////
 
+# O'zgaruvchilar
+
 # ism = "Abdulloh"
 # yosh = 25
 # print(ism)
@@ -43,22 +45,104 @@ import math
 # print(c)
 
 # "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
-text = "hello world!"
-print(text)
+# text = "hello world!"
+# print(text)
 
 # xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
-xabar = "bugun havo ochiq boldi"
-print(xabar)
-xabar = 'kechga borib yomg\'ir yog\'di'
-print(xabar)
+# xabar = "bugun havo ochiq boldi"
+# print(xabar)
+# xabar = 'kechga borib yomg\'ir yog\'di'
+# print(xabar)
 # class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
 # class = "11-a"
 # print(class)
 
-radius = 5
-pi = 3.14159
-aylana_yuzi = pi * radius**2
-print("Radiusi", radius, "ga teng aylananing yuzi=", aylana_yuzi)
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi", radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+# ////////////
+
+# Matn bilan ishlash (string)
+
+# ism = "Ayubxon"
+# shahar = "Qoqon"
+# viloyat = "Farg'ona"
+# matn = "men yangi 📱 oldim"
+# smayl = '😊'
+
+
+# ism = "Ahmad"
+# print("Mening ismim " + ism)
+
+# ism = "Ahad"
+# familya = "Qayum"
+# print(ism + familya)
+# print(ism +' '+ familya)
+
+
+# ism = "Ahad"
+# familya = "Qayum"
+# ism_sharif = f"{ism} {familya}"
+# print(ism_sharif)
+
+# ism = "James"
+# familya = "Bond"
+# print(f'Salom mening ismim {familya}. {ism} {familya}')
+
+# print("hello \tworld")
+
+# print("hello \nworld")
+
+# ism = "james"
+# familya = "bond"
+# ism_sharif = f"{ism} {familya}"
+# ism_sharif = ism_sharif.upper()
+
+# print(ism_sharif.lower())
+# print(ism_sharif.title())
+# print(ism_sharif.capitalize())
+
+# meva = "    olma      "
+# print('men ' + meva.lstrip()+ "yaxshi ko'raman")
+# print('men ' + meva.rstrip()+ " yaxshi ko'raman")
+# print('men ' + meva.strip()+ " yaxshi ko'raman")
+
+
+# ism = input("ismingizni kiriting ")
+# print("Assalomu aleykum " + ism.title())
+
+kocha = input("qaysi ko'chada turasiz ")
+mahalla = input('qaysi mahallada turasiz ')
+tuman = input("qaysi tumanda turasiz ")
+viloyat = input("qaysi viloyatdansiz ")
+manzil = (f"{kocha} ko'chasi, \n{mahalla} mahallasi, \n{tuman} tumani, \n{viloyat} viloyati")
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.title())
+print(manzil.capitalize())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
