16bd864303bc45912a1f985d8282c4ee845bda7d
Ayubxon Axmatxonov <114375261+ayubxon1775@users.noreply.github.com>
2025-11-23 14:11:47 +0500

    added learn task

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 22 11:14:53 2025
+
+@author: Админ
+"""
+
+# sana 23.11.2025
+import math
+
+print ('"Nexia", "Tiko", "Damas" ko\'rganlar havas qilar')
+# 5 ning 4-darajasini toping
+print('5 ning 4-darajasi', 5**4)
+# 22 ni 4 ga bolganda qancha qoldiq qoladi
+print('22 ni 4 ga bolsa', 22/4 ,'qoldiq chiqadi')
+# tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("tomonlari 125 ga teng kvadratning yuzi", 125*125, "perimetri", 125*4)
+# diametri 12 teng bolgan doiraning yuzini toping
+print('diametri 12 teng bolgan doiraning yuzi', 3.14*(12/2)**2)
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", math.sqrt((6*6) + (7*7)))
